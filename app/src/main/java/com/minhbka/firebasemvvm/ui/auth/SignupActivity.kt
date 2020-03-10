25ee7444d75c0242416fcdd1c8f3ba8e59faacbc
Phan Minh <minhpv@kormedi.com>
2020-03-10 13:53:37 +0900

    - Adding Required Dependencies - Adding Firebase Authentication - Enabling Data Binding - Creating Packages - Creating Activities

--- app/src/main/java/com/minhbka/firebasemvvm/ui/auth/SignupActivity.kt
+++ app/src/main/java/com/minhbka/firebasemvvm/ui/auth/SignupActivity.kt
@@ -1,12 +1,13 @@
-package com.minhbka.firebasemvvm
+package com.minhbka.firebasemvvm.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.minhbka.firebasemvvm.R
 
-class MainActivity : AppCompatActivity() {
+class SignupActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_signup)
     }
 }
---
+++ app/src/main/java/com/minhbka/firebasemvvm/ui/home/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.minhbka.firebasemvvm.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.minhbka.firebasemvvm.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
